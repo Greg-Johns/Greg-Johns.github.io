e59f9c8ff1262bf9d3d3b34ae71e90ac92f57b75
Greg Johns <Greg.johns@gmail.com>
2018-01-05 14:26:56 -0600

    Updates

--- path---about-fdf2d03ea32cc49dec93.js
+++ path---about-fdf2d03ea32cc49dec93.js
@@ -1,2 +1,2 @@
-webpackJsonp([0xf927f8900006],{365:function(t,a){t.exports={data:{site:{siteMetadata:{title:"GREG JOHNS"}}},pathContext:{}}}});
+webpackJsonp([0xf927f8900006],{364:function(t,a){t.exports={data:{site:{siteMetadata:{title:"GREG JOHNS"}}},pathContext:{}}}});
 //# sourceMappingURL=path---about-fdf2d03ea32cc49dec93.js.map
