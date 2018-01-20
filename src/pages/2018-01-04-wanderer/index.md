5fa01b7cd4a7047787dc8c39e58719756252c9a2
Greg Johns <greg.johns@gmail.com>
2018-01-19 18:17:24 -0600

    portofilo and spelling corrections

--- src/pages/2018-01-04-wanderer/index.md
+++ src/pages/2018-01-04-wanderer/index.md
@@ -1,18 +1,16 @@
 ---
-title: Wanderer
+title: Wandering
 date: "2018-01-04"
 path: "/wandering/"
 ---
 
-So much for staying on path and focused. It's been over two year's since that writing that "stay focused", "write more" crap and I only wrote one more post after that. Then nothing...
+So much for staying up with my blogging efforts. It's been over two years since my last post and after all that time working out the perfect build and deployment process I got such little use out of actually using it. No more posts, no follow through, no conviction, nothing. Once I feel I've solved the design/development problems on something my attention wanders off looking for something else to tackle. The only reason i'm writing this now is because I found a yet another, newer blogging engine that I wanted to try so out with the old ~~Metalsmith~~ and in with the new [Gatsby](https://www.gatsbyjs.io). 
 
-#### The timing wasn't right
+This isn't because of any issue with Metalsmith, I very much like it and still used it for setting up quick process pipelines for various things at work. They have a ton of plug-ins and good developer community that keeps it fresh. I would still use it for it's quick simple ways of setting up transformation pipelines. Gatsby on the other hand is more for quick setup of React based projects and not as flexible for a general of build tool. The main reason to make the switch was more because I was looking to get a personal React project going and had been following the progress of Gatsby for a while now and liked the direction it was going.
 
-I'm going to blame it on **time** which I've never seemed to catch enough of as it slips by. But this time it's  different. I can feel it. I'm going to write more.
+Gatsby uses a React component based front end, GraphQL data fetching and other features that I was interested in working with so I've converted the site over and worked up a re-designed to keep it fresh. 
 
-Also, I got a newer blogger engine thing, with more new coolness. That's right, no more ~~Metalsmith~~ it's all about [Gatsby](https://www.gatsbyjs.io) now that uses the Webpack build tool, React component based front end, GraphQL data fetching and more stuff too. But this isn't about any issue with Metalsmith, I very much liked it and used it for setting up quick process pipelines for various things at work. They have a ton of plugins and good developer community that keeps it fresh. I would still use it for it's quick and simple way to set up transformation pipelines. Gatsby as I said uses Webpack for the build process with is notoriously more difficult and opinionated not to mention Gatsby more for quick setup of React based project and not as flexible or general of tool. The main reason to make the switch was more because I was looking to get a personal React project going and had been following the progress of Gatsby for a while now and liked the direction it was going.
-
-We just celebrated the new year a few days ago when people look back through time while equally thinking about the new year to come. I would imagine that "write more" is a common resolve for people that do such things. New years resolution always seem like a self-esteem trap and I resolve not to make any. Ok, any but one. I resolve to **try** to catch more of that slippery time and write more.
+It's also a new year, a time when people look back and reassess, set resolutions and re-focus priorities. I would imagine that "write more" is a common resolve for some as it has been with me but new years resolution always seem like a self-esteem trap so this year I'll settle for just **trying** to write more.
 
 <figure class='mdFigure'>
   <img src='./ant_rotated.png' alt="ant" />
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -3,6 +3,9 @@ import Link from 'gatsby-link'
 import '../css/styles.css'
 import get from 'lodash/get'
 import Helmet from 'react-helmet'
+import remoteRendering from './rcr.jpg'
+import remoteSketches from './rcs.jpg'
+import remoteInteractions from './ri.jpg'
 import Footer from '../components/Footer'
 
 const breakerBreaker = {
@@ -23,55 +26,19 @@ class BlogPortfolio extends React.Component {
         <article>
           <h2>~ Portfolio ~</h2>
 
-          <p className='intro'>I have over two decades of experience innovating in the digital media industries. I have managed and consulted for corporations of all shapes and sizes, developed a variety of pioneering online services, B2B systems, SAAS systems, and hold 9 patents. I've helped small business owners to big CEO's take their idea from the back of napkin or multi-page BRD to their online dream. I've worked with stakeholders to shake out ideas, define information spaces, identify design patterns, understanding users, wireframe mockups, prototype, test, iterate, iterate, iterate, build teams, build processes, build users and realize their next new thing. I even alternate between electric and manual tooth brush every other night because I think it makes my teeth cleaner.</p>
+          <p className='intro'>I have over two decades of experience innovating in the digital media industries. I have managed and consulted for corporations of all shapes and sizes, developed a variety of pioneering online services, B2B systems, SAAS systems, and hold 11 patents. I've helped small business owners and big CEO's take their idea from the back of napkin or multi-page business requirement document to their online dream. I've worked with stakeholders to shake out ideas, define information spaces, identify design patterns, understand their users, wireframe mockups, prototype, test, iterate, build teams, build processes, build users and realize their next new thing. I even alternate between electric and manual toothbrush every other night because I think it makes my teeth cleaner.</p>
 
-          <p>For those currious about my thoughts and approach to developing software or computers I have a few related blog posts you can read or skip over to my Github, NPM profiles. For those looking for more of the conceptual/design side of software what follows are a few case studdies of some of my UI/UX work.</p>
+            <p>For those currious about my thoughts and approach to developing software or computers I have a few related blog posts you can read or skip over to my Github, NPM profiles. For those looking for more of the conceptual/design side of software the follows are a few case studies of some of my UI/UX work.</p>
 
-          <h2 style={breakerBreaker}> ~ * ~ </h2>
+          <h2 style={breakerBreaker}> ~ Case studies comming soon ~ </h2>
 
-          <ul className='listLinks'>
-            <li>
-                - Lanvera customer client document managment concept (blog post)
-            </li>
-            <li>
-                - Avaya QPT (blog post)
-            </li>
-            <li>
-                - Avaya Scopiafy concept/proto (blog post)
-            </li>
-            <li>
-                - FedEx IA and concept wires (blog post)
-            </li>
-            <li>
-                - Supermarket data concept (blog post)
-            </li>
-            <li>
-                - Verizon FiOS Interaction design and prototypes
-                        MultiLevelImgSearch (blog post)
-                        TimeWinder (blog link)
-                        FiOS concept & early prototype work
-                        FiOS V.X
-                        Remote UX
-            </li>
-            <li>
-                - Widget work (WizProgBar, CSSFramed, DashboardDefault, BookmarBlog)
-            </li>
-            <li>
-                Old site & logo sliders
-            </li>
-          </ul>
-
-          <p>If your just looking for my work resume you can view a print friendly, html resume online viersion or because some people still like to download, attach to emails, print out and fill away I also provide a handy button to Download Resume</p>
 
           <ul className='listLinks'>
             <li>
-              <a href='http://www.gregjohns.com/cv'>Online Resume</a>
+              <a href='http://www.gregjohns.com/cv'>Resume</a>
             </li>
             <li>
-              <a href='https://'>Download Resume</a>
-            </li>
-            <li>
-              <a href='https://patents.google.com/?inventor=Greg+Johns'>List of pattents</a>
+              <a href='https://patents.google.com/?inventor=Greg+Johns'>Pattents</a>
             </li>
           </ul>
         </article>
